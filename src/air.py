8393be7f6f134f088b19aa5c2f9e202ee79ae174
PharaohCola13 <academic@sriley.dev>
2019-04-02 10:48:38 -0600

    update

--- src/air.py
+++ src/air.py
@@ -8,24 +8,24 @@ labels  = []
 read    = data.readlines()
 # Pulls the air temperature that are not labeled as overcast
 def overcast():
-	domain		= []
-	overcast1 	= []
-	overcast2 	= []
-	overcast3 	= []
-	c 		= [z.split(',') for z in read]
-	del c[0]
-	for j in c:
-		if j[11] != 'overcast':
-			domain.append(j[0])
-			overcast1.append(j[2])
-			overcast2.append(j[4])
-			overcast3.append(j[6])
-		else:
-			continue
-	y1 = overcast1
-	y2 = overcast2
-	y3 = overcast3
-	return domain, y1,y2,y3
+    domain		= []
+    overcast1 	= []
+    overcast2 	= []
+    overcast3 	= []
+    c 		= [z.split(',') for z in read]
+    del c[0]
+    for j in c:
+        if j[11] != 'overcast':
+            domain.append(j[0])
+            overcast1.append(j[2])
+            overcast2.append(j[4])
+            overcast3.append(j[6])
+        else:
+            continue
+    y1 = overcast1
+    y2 = overcast2
+    y3 = overcast3
+    return domain, y1,y2,y3
 
 x,y1,y2,y3 	= overcast()
 range1 = [float(line) for line in y1]
