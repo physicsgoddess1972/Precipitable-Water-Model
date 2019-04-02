22c625aae12d6db87e8ee502c0a9d2864b250d67
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-04-02 10:18:31 -0600

    update

---
+++ src/air.py
@@ -0,0 +1,47 @@
+import matplotlib
+from  matplotlib import pyplot as plt
+from numpy import *
+
+fname   = "./master_data.csv"
+data    = open(fname, 'r')
+labels  = []
+read    = data.readlines()
+# Pulls the air temperature that are not labeled as overcast
+def overcast():
+	domain		= []
+	overcast1 	= []
+	overcast2 	= []
+	overcast3 	= []
+	c 		= [z.split(',') for z in read]
+	del c[0]
+	for j in c:
+		if j[11] != 'overcast':
+			domain.append(j[0])
+			overcast1.append(j[2])
+			overcast2.append(j[4])
+			overcast3.append(j[6])
+		else:
+			continue
+	y1 = overcast1
+	y2 = overcast2
+	y3 = overcast3
+	return domain, y1,y2,y3
+
+x,y1,y2,y3 	= overcast()
+range1 = [float(line) for line in y1]
+range2 = [float(line) for line in y2]
+range3 = [float(line) for line in y3]
+
+plt.xlabel("Date")
+plt.xticks(arange(0,len(x), 7), x, rotation=30, fontsize='small', horizontalalignment='center')
+plt.subplots_adjust(bottom=0.16)
+plt.title("Air Temperature")
+
+plt.scatter(arange(0,len(x)),array(range1),color='crimson')
+plt.scatter(arange(0,len(x)),array(range2),color='deepskyblue')
+plt.scatter(arange(0,len(x)),array(range3), color='green')
+
+plt.ylabel("Temperature [C]")
+
+plt.legend(loc='upper left')
+plt.show()
