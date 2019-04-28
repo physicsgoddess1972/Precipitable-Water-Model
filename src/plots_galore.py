84e89c5be721ebbbaaf830ee32618fe26251b642
Spencer Riley <spencer.riley@student.nmt.edu>
2019-04-28 13:33:01 -0600

    update

--- src/plots_galore.py
+++ src/plots_galore.py
@@ -35,7 +35,7 @@ def overcast():
 	overcast3 	= []
 	overcast4 	= []
 	overcast5 	= []
-	overcast6 	= []
+	#overcast6 	= []
 	c 		= [z.split(',') for z in read]
 	del c[0]
 	for j in c:
@@ -45,7 +45,7 @@ def overcast():
 			overcast3.append(j[8])
 			overcast4.append(j[9])
 			overcast5.append(j[10])
-			overcast6.append(j[15])
+	#		overcast6.append(j[15])
 		else:
 			continue
 	y1 = overcast1
@@ -53,8 +53,8 @@ def overcast():
 	y3 = overcast3
 	y4 = overcast4
 	y5 = overcast5
-	y6 = overcast6
-	return y1,y2,y3,y4,y5,y6
+	#y6 = overcast6
+	return y1,y2,y3,y4,y5#,y6
 
 ## Creates an exponential fit
 def trendline(x, input, n):
@@ -96,8 +96,8 @@ def trendline(x, input, n):
 	r2          = 1 - (ss_res/ss_tot)
 	return t, avg, yy, t1,popt,r2, residuals
 
-y1,y2,y3,y4,y5,y6 = overcast()
-print(y2)
+y1,y2,y3,y4,y5 = overcast()
+
 x           = array([float(line) for line in y1])
 y1_abq1     = array([float(line) for line in y2])
 y1_abq2     = array([float(line) for line in y3])
@@ -107,7 +107,7 @@ y2_epz2     = array([float(line) for line in y5])
 y1_abq      = (y1_abq1+y1_abq2)/2
 y2_epz      = (y2_epz1+y2_epz2)/2
 super_avg   = (y1_abq1+y1_abq2+y2_epz1+y2_epz2)/4
-rel_humid 	= array([float(line) for line in y6])
+#rel_humid 	= array([float(line) for line in y6])
 
 # Logged
 t_log1, avg_log1, yy_log1, t1_log1, popt_log1, r2_log1, res_log1    = trendline(x, log1p(super_avg), "Ln(1+x)")
@@ -144,9 +144,9 @@ plt.legend(loc="upper right")
 plt.figure(3)
 plt.subplots_adjust(bottom=0.16)
 plt.scatter(t,log1p(avg),color='blueviolet')
-plt.plot(t1_log1, yy_log1, color='limegreen',label='ln(%2.3f)$ + %2.3f x$\t|\tln(x + 1)' % tuple(popt_log1))
+#plt.plot(t1_log1, yy_log1, color='limegreen',label='ln(%2.3f)$ + %2.3f x$\t|\tln(x + 1)' % tuple(popt_log1))
 plt.plot(t1_log2, yy_log2, color='mediumblue',label='ln(%2.3f)$ + %2.3f x$\t|\tln(x)' % tuple(popt_log2))
-plt.plot(t1, log(25.08) + 0.041*t1, color="crimson")
+#plt.plot(t1, log(25.08) + 0.041*t1, color="crimson")
 plt.title("Correlation between Logged Mean Precipitable Water and Temperature")
 plt.xlabel("Zenith Sky Temperature [C]")
 plt.ylabel("ln(PW) [mm]")
@@ -156,9 +156,7 @@ plt.figure(4)
 plt.subplots_adjust(bottom=0.16)
 plt.scatter(t,avg,color='blueviolet')
 plt.plot(t1, yy, color='limegreen',label=r'%2.3f$e^{%2.3f x}$' % tuple(popt))
-# plt.plot(t1,exp(yy_log1), color="limegreen", label='%2.3f$e^{%2.3f x}$\t|\tln(x + 1)' % tuple(popt_log1))
-# plt.plot(t1,exp(yy_log2), color="mediumblue", label='%2.3f$e^{%2.3f x}$\t|\tln(x)' % tuple(popt_log2))
-# plt.plot(t1, 25.08 * exp(0.041*t1), color="crimson")
+plt.plot(t1, 25.08 * exp(0.041*t1), color="crimson")
 plt.title("Correlation between Mean Precipitable Water and Temperature")
 plt.xlabel("Zenith Sky Temperature [C]")
 plt.ylabel("PW [mm]")
@@ -168,7 +166,7 @@ plt.legend(loc="lower right")
 plt.figure(5)
 plt.subplots_adjust(bottom=0.16)
 plt.scatter(t_log1,res_log1,color='limegreen')
-plt.scatter(t_log2,res_log2,color='mediumblue')
+#plt.scatter(t_log2,res_log2,color='mediumblue')
 plt.title("Residual Plot for the Logged Mean Precipitable Water and Temperature")
 plt.xlabel("Zenith Sky Temperature [C]")
 plt.ylabel("$\sigma$")
