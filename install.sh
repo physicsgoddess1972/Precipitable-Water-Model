31a740800e5a73d6339c980515b4ad2d631b4a96
PharaohCola13 <academic@sriley.dev>
2019-05-19 23:35:31 -0600

    update

--- install.sh
+++ install.sh
@@ -4,8 +4,5 @@ sudo apt-get install r-base
 
 ## R Package Requirements
 sudo su - -c "R -e \"install.packages('plotrix', repos='https://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('tcltk', repos='https://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('tkrplot', repos='https://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('nlstools', repos='https://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
