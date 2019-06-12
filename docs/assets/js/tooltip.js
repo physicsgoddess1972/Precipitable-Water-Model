b67ee0bf5b6ebad43eb65fa915a080c92bb2fb13
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-06-12 16:21:44 -0600

    updates to docs

--- docs/assets/js/tooltip.js
+++ docs/assets/js/tooltip.js
@@ -5,7 +5,7 @@ tippy('#bug-report', {
     interactive: true,
     placement: 'left',
     theme: 'ocean',
-    content:"Bug report"
+    content:"Bug report/Feature Request"
 })
 
 tippy('#git', {
@@ -46,5 +46,24 @@ tippy('#contribute', {
     placement: 'left',
     theme: 'ocean',
     content:"Contribute to the Project"
+})
+
+tippy('#home', {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'left',
+    theme: 'ocean',
+    content:"Return Home"
+})
+tippy('#code', {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'left',
+    theme: 'ocean',
+    content:"R Features"
 })
 
