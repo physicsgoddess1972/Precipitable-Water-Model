9b240474f852704eb2a69f8e141cce737ed289cd
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-06-16 18:55:30 -0600

    update

--- docs/assets/js/modal1.js
+++ docs/assets/js/modal1.js
@@ -1,12 +1,8 @@
-
-$( document ).ready(function() {
+	$( document ).ready(function() {
     if (document.cookie.indexOf('visited=true') == -1){
-        // load the overlay
-        $('#modal1').modal('open');
-
+       $('.modal').modal('open');
        var year = 1000*60*60*24*365;
-        var expires = new Date((new Date()).valueOf() + year);
-        document.cookie = "visited=true;expires=" + expires.toUTCString();
-
+       var expires = new Date((new Date()).valueOf() + year);
+       document.cookie = "visited=true;expires=" + expires.toUTCString();
     }
 });
