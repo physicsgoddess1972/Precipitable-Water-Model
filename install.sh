62ac8018b4edf4b619e3798f4ade5ae6bdeb08b8
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-06-30 21:11:58 -0600

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 ## System Requirements
-sudo apt-get install r-base
+sudo apt-get install r-base libv8-dev
 
 ## R Package Requirements
-sudo su - -c "R -e \"install.packages('plotrix', repos='https://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('randomcoloR', repos='https://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
