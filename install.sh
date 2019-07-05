aec26b6e535a2f1ea4f842695d1cbb27f6508f2f
PharaohCola13 <academic@sriley.dev>
2019-07-05 10:13:45 -0600

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 ## System Requirements
-sudo apt-get install r-base libv8-dev
+apt-get install r-base libv8-dev python-dev python-matplotlib
 
 ## R Package Requirements
-sudo su - -c "R -e \"install.packages('randomcoloR', repos='https://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
+su - -c "R -e \"install.packages('randomcoloR', repos='https://cran.rstudio.com/')\""
+su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/')\""
+su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
+su - -c "R -e \"library(devtools)\""
+su - -c "R -e \"install_github('mpastell/Rpyplot)\""
