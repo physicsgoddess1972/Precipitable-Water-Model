e1e8344e210c58e9bf9f037972b6ff8d840aecf2
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-08-06 07:04:39 -0600

    update

--- src/modtran.r
+++ src/modtran.r
@@ -1,4 +1,4 @@
 ## Imports data from master_data.csv
 fname       <- read.table(file="../data/modtran.csv", sep=",", header=TRUE, strip.white=TRUE)
-plot(fname[,2], fname[,4], pch=16, xlab=sprintf("Wavelength (microns)"), ylab="Radiance (W/CM2-STER-MICRN)",
-main="MODTRAN Plot")
+plot(fname[,2], fname[,4]*pi*10000, pch=16, xlab=sprintf("Wavelength (microns)"), ylab="Radiance (W/M2-MICRN)",
+main="MODTRAN Plot", cex=0.6)
