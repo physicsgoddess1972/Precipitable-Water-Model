db9b333fea27de83f519830d3e6c484b221995af
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-08-07 17:20:28 -0600

    update

--- src/archive/data_set.py
+++ src/archive/data_set.py
@@ -1,8 +1,8 @@
 from numpy import *
 
-fname1 = "../../data/modtran1.csv"
-fname2 = "../../data/modtran2.csv"
-sname = "../../data/modtran.csv"
+fname1 = "../../data/modtran/modtran1.csv"
+fname2 = "../../data/modtran/modtran2.csv"
+sname = "../../data/modtran/modtran.csv"
 wl  = loadtxt(fname1, delimiter=",", unpack=True, skiprows=3)[1]
 rd1 = loadtxt(fname1, delimiter=",", unpack=True, skiprows=3)[3]
 rd2 = loadtxt(fname2, delimiter=",", unpack=True, skiprows=3)[3]
@@ -16,7 +16,6 @@ for i in range(0, len(wl)):
     f.write(",")
     f.write(str(rd2[i]))
     f.write("\n")
-
 f.close()
 
 
