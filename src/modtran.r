dab0ae83e53fb207c81d0ca6eac62d464326fb2b
Spencer Riley <spencer.riley@student.nmt.edu>
2019-08-11 20:24:37 -0600

    update

--- src/modtran.r
+++ src/modtran.r
@@ -1,12 +1,23 @@
 ## Imports data from master_data.csv
-fname       <- read.table(file="../data/modtran.csv", sep=",", header=TRUE, strip.white=TRUE)
+fname       <- read.table(file="../data/modtran/modtran.csv", sep=",", header=TRUE, strip.white=TRUE)
 rad <- function(data){
     intensity <- data * pi * 10^4
     return(intensity)
 }
 pdf("~/Downloads/modtran.pdf")
-plot(fname[,1], rad(fname[,2]), pch=16, xlab=sprintf("Wavelength (microns)"), ylab="Intensity (W/M2-MICRN)",
-main="MODTRAN Plot", cex=0.6, col="#FF9300", xlim=c(5, 30))
-points(fname[,1], rad(fname[,3]), pch=16, cex=0.6, col="#006CFF")
+plot(fname[,1], rad(fname[,2]), type='n', xlab=NA, ylab=NA,
+main="MODTRAN Plot", xlim=c(5, 30))
+lines(fname[,1], rad(fname[,2]), cex=0.6, col="#FF9300")
+lines(fname[,1], rad(fname[,3]), cex=0.6, col="#006CFF")
+mtext(bquote("Intensity (W/"*m^2*mu*m*")"), side=2, line=2)
+mtext(bquote("Wavelength ("*mu*m*")"), side=1, line=3)
+
+# c = (2.998*10^8)*(10^6)
+# h = (6.626*10^-34)
+# k = (1.38*10^-23)
+# T = 280
+# print(exp((-h*c)/(5:10 * k * T)))
+# curve(exp((-h*c)/(x * k * T)), add=TRUE)
+
 legend("topright", col=c("#FF9300","#006CFF"), pch=c(16,16), legend=c("Water Vapor Scale = 1","Water Vapor Scale = 2"))
 graphics.off()
