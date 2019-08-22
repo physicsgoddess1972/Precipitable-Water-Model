b14922d97c4d199e28c5318c2287f2b4092d43e9
Spencer Riley <spencer.riley@student.nmt.edu>
2019-08-21 19:47:34 -0600

    update

--- src/modtran.r
+++ src/modtran.r
@@ -23,10 +23,30 @@ modtran_plot <- function(xran){
     curve(planck_curve(x, 240), col=color[3], add=TRUE)
     curve(planck_curve(x, 280),col=color[4], add=TRUE)
 
-    legend("topright", col=color, pch=c(16,16), legend=c("Water Vapor Scale = 1","Water Vapor Scale = 2", "T = 240 K", "T = 280 K"))
+    avg_1 <- avg_2 <- avg_0 <- list()
+    for(i in 1:length(fname[,1])){
+        if(fname[i,1] >= xran[1]){
+            if(fname[i,1] <= xran[2]){
+                avg_0 <- append(avg_0, fname[i,1])
+                avg_1 <- append(avg_1, rad(fname[i,2]))
+                avg_2 <- append(avg_2, rad(fname[i,3]))
+            }
+        }
+    }
+    avg0 <- Reduce("+", avg_0)/length(avg_0)
+    avg1 <- Reduce("+", avg_1)/length(avg_1)
+    avg2 <- Reduce("+", avg_2)/length(avg_2)
+
+    points(avg0, avg1, pch=16, col=color[1])
+    points(avg0, avg2, pch=16, col=color[2])
+    points(avg0, Reduce("+", planck_curve(xran, 240))/length(planck_curve(xran, 240)), pch=16, col=color[3])
+    points(avg0, Reduce("+", planck_curve(xran, 280))/length(planck_curve(xran, 280)), pch=16, col=color[4])
+
+    legend("topright", col=color, pch=c(16,16),
+        legend=c("Water Vapor Scale = 1","Water Vapor Scale = 2", "T = 240 K", "T = 280 K"))
 }
 pdf("~/Downloads/modtran.pdf")
-modtran_plot(c(5,25))
+#modtran_plot(c(5,25))
 
 modtran_plot(c(7, 10))
 graphics.off()
