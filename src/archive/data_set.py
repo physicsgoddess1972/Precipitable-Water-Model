8a0940ca9b85cdb18ea45411c0070504855eaee4
Spencer Riley <spencer.riley@student.nmt.edu>
2019-09-15 16:24:42 -0600

    update

--- src/archive/data_set.py
+++ src/archive/data_set.py
@@ -1,31 +1,35 @@
 from numpy import *
 
-fname1 = "../../data/modtran/modtran1.csv"
-fname2 = "../../data/modtran/modtran2.csv"
-fname3 = "../../data/modtran/modtran3.csv"
-sname = "../../data/modtran/modtran.csv"
+fname1 = "../../data/modtran/radiance/modtran1.csv"
+fname2 = "../../data/modtran/radiance/modtran2.csv"
+fname3 = "../../data/modtran/radiance/modtran3.csv"
+fname4 = "../../data/modtran/radiance/modtran4.csv"
+sname = "../../data/modtran/radiance/modtran.csv"
 
-fname4 = "../../data/modtran/temp_offset/modtran-5.csv"
-#fname5 = "../../data/modtran/temp_offset/modtran-2.csv"
-fname5 = "../../data/modtran/temp_offset/modtran0.csv"
-#fname7 = "../../data/modtran/temp_offset/modtran+2.csv"
-fname6 = "../../data/modtran/temp_offset/modtran+5.csv"
-sname1 = "../../data/modtran/temp_offset/modtran.csv"
+# fname4 = "../../data/modtran/temp_offset/modtran-5.csv"
+# #fname5 = "../../data/modtran/temp_offset/modtran-2.csv"
+# fname5 = "../../data/modtran/temp_offset/modtran0.csv"
+# #fname7 = "../../data/modtran/temp_offset/modtran+2.csv"
+# fname6 = "../../data/modtran/temp_offset/modtran+5.csv"
+# sname1 = "../../data/modtran/temp_offset/modtran.csv"
 
-wl  = loadtxt(fname4, delimiter=",", unpack=True, skiprows=3)[1]
-rd1 = loadtxt(fname4, delimiter=",", unpack=True, skiprows=3)[3]
-rd2 = loadtxt(fname5, delimiter=",", unpack=True, skiprows=3)[3]
-rd3 = loadtxt(fname6, delimiter=",", unpack=True, skiprows=3)[3]
-f = open(sname1, "w")
-f.write("wavelength, Radiance (T-2), Radiance (T), Radiance (T+2)\n")
+wl  = loadtxt(fname1, delimiter=",", unpack=True, skiprows=3)[1]
+rd1 = loadtxt(fname1, delimiter=",", unpack=True, skiprows=3)[3]
+rd2 = loadtxt(fname2, delimiter=",", unpack=True, skiprows=3)[3]
+rd3 = loadtxt(fname3, delimiter=",", unpack=True, skiprows=3)[3]
+rd4 = loadtxt(fname4, delimiter=",", unpack=True, skiprows=3)[3]
+f = open(sname, "w")
+f.write("wavelength, Radiance (WVS 0.5), Radiance (WVS 1.0), Radiance (WVS 1.5), Radiance (WVS 2.0)\n")
 for i in range(0, len(wl)):
     f.write(str(wl[i]))
     f.write(",")
+    f.write(str(rd3[i]))
+    f.write(",")
     f.write(str(rd1[i]))
     f.write(",")
-    f.write(str(rd2[i]))
+    f.write(str(rd4[i]))
     f.write(",")
-    f.write(str(rd3[i]))
+    f.write(str(rd2[i]))
     f.write("\n")
 f.close()
 
--- src/modtran.r
+++ src/modtran.r
@@ -2,12 +2,91 @@
 fname1       <- read.table(file="../data/modtran/radiance/modtran.csv", sep=",", header=TRUE, strip.white=TRUE)
 fname2       <- read.table(file="../data/modtran/temp_offset/modtran.csv", sep=",", header=TRUE, strip.white=TRUE)
 fname3       <- read.table(file="../data/modtran/modtran_wps1.csv", sep=",", header=TRUE, strip.white=TRUE)
+
 modtran_plot <- function(xran, fname,offset, leg) {
     rad <- function(data){
         intensity <- data * pi * 10^4
         return(intensity)
     }
-    color = c("#00BCD7", "#FF9A00", "#66FF33", "#D90000", "#8F00C9")
+    color = c("#00BCD7", "#FF9A00", "#66FF33", "#B426A5" ,"#D90000", "#005E8D")
+    plot(fname[,1], rad(fname[,2]), type='n', xlab=NA, ylab=NA,
+    main="Wavelength and Intensity Correlation", xlim=xran, ylim=c(0, 30))
+    lines(fname[,1], rad(fname[,2]), cex=0.6, col=color[1])
+    lines(fname[,1], rad(fname[,3]), cex=0.6, col=color[2])
+    lines(fname[,1], rad(fname[,4]), cex=0.6, col=color[3])
+    lines(fname[,1], rad(fname[,5]), cex=0.6, col=color[4])
+    mtext(bquote("Intensity (W/"*m^2*mu*m*")"), side=2, line=2)
+    mtext(bquote("Wavelength ("*mu*m*")"), side=1, line=3)
+
+    planck_curve <- function(x, T){
+        c1  = 1.191042*10^8
+        c2  = 1.4387752*10^4
+        B   = c1/(x^5*(exp(c2/(x*T)) - 1)) * pi
+        return(B)
+    }
+    temp_planck_curve <- function(x, B){
+        c1  = 1.191042*10^8
+        c2  = 1.4387752*10^4
+        t   = (x/c2) * log(((c1*pi)/(B*x^5)) + 1)
+        T   = 1/t
+        return(T)
+    }
+
+    avg_3 <- avg_1 <- avg_2 <- avg_0 <- avg_4 <- list()
+    for(i in 1:length(fname[,1])){
+        if(fname[i,1] >= xran[1]){
+            if(fname[i,1] <= xran[2]){
+                avg_0 <- append(avg_0, fname[i,1])
+                avg_1 <- append(avg_1, rad(fname[i,2]))
+                avg_2 <- append(avg_2, rad(fname[i,3]))
+                avg_3 <- append(avg_3, rad(fname[i,4]))
+                avg_4 <- append(avg_4, rad(fname[i,5]))
+            }
+        }
+    }
+    avg0 <- Reduce("+", avg_0)/length(avg_0)
+    avg1 <- Reduce("+", avg_1)/length(avg_1)
+    avg2 <- Reduce("+", avg_2)/length(avg_2)
+    avg3 <- Reduce("+", avg_3)/length(avg_3)
+    avg4 <- Reduce("+", avg_4)/length(avg_4)
+
+    curve(planck_curve(x, temp_planck_curve(avg0, avg1)), add=TRUE)
+    curve(planck_curve(x, temp_planck_curve(avg0, avg2)), add=TRUE)
+    curve(planck_curve(x, temp_planck_curve(avg0, avg3)), add=TRUE)
+    curve(planck_curve(x, temp_planck_curve(avg0, avg4)), add=TRUE)
+
+    points(avg0, avg1, pch=16, col=color[1])
+    points(avg0, avg2, pch=16, col=color[2])
+    points(avg0, avg3, pch=16, col=color[3])
+    points(avg0, avg4, pch=16, col=color[4])
+
+    curve(planck_curve(x, 240),col=color[5], add=TRUE)
+    curve(planck_curve(x, 280),col=color[6], add=TRUE)
+
+    points(avg0, Reduce("+", planck_curve(xran, 280))/length(planck_curve(xran, 280)), pch=16, col=color[6])
+    points(avg0, Reduce("+", planck_curve(xran, 240))/length(planck_curve(xran, 240)), pch=16, col=color[5])
+
+    text(8.8, 21.5, label="T = 280 K", col=color[6], srt=10)
+    text(7.5, 5.85, label="T = 240 K", col=color[5], srt=10)
+
+    text(9.5,offset[1], label=paste("T = ", round(temp_planck_curve(avg0, avg1),2), "K"), srt=5, cex=0.75)
+    text(9.5,offset[2], label=paste("T = ", round(temp_planck_curve(avg0, avg2),2), "K"), srt=5, cex=0.75)
+    text(9.5,offset[3], label=paste("T = ", round(temp_planck_curve(avg0, avg3),2), "K"), srt=5, cex=0.75)
+    text(9.5,offset[4], label=paste("T = ", round(temp_planck_curve(avg0, avg4),2), "K"), srt=5, cex=0.75)
+    legend("topright", col=color, pch=c(16,16), legend=leg)
+}
+
+modtran_plot1 <- function(fname){
+    plot(fname[,5], fname[,2], xlab="Water Pressure (mbar)", ylab="Altitude (km)",
+    main="Altitude and Water Pressure Correlation")
+}
+
+modtran_plot2 <- function(xran, fname,offset, leg){
+        rad <- function(data){
+        intensity <- data * pi * 10^4
+        return(intensity)
+    }
+    color = c("#00BCD7", "#FF9A00", "#66FF33","#D90000", "#005E8D")
     plot(fname[,1], rad(fname[,2]), type='n', xlab=NA, ylab=NA,
     main="Wavelength and Intensity Correlation", xlim=xran, ylim=c(0, 30))
     lines(fname[,1], rad(fname[,2]), cex=0.6, col=color[1])
@@ -29,10 +108,8 @@ modtran_plot <- function(xran, fname,offset, leg) {
         T   = 1/t
         return(T)
     }
-    curve(planck_curve(x, 240), col=color[4], add=TRUE)
-    curve(planck_curve(x, 280),col=color[5], add=TRUE)
 
-    avg_3 <- avg_1 <- avg_2 <- avg_0 <- list()
+    avg_3 <- avg_1 <- avg_2 <- avg_0 <- avg_4 <- list()
     for(i in 1:length(fname[,1])){
         if(fname[i,1] >= xran[1]){
             if(fname[i,1] <= xran[2]){
@@ -56,25 +133,23 @@ modtran_plot <- function(xran, fname,offset, leg) {
     points(avg0, avg2, pch=16, col=color[2])
     points(avg0, avg3, pch=16, col=color[3])
 
-    points(avg0, Reduce("+", planck_curve(xran, 240))/length(planck_curve(xran, 240)), pch=16, col=color[4])
+    curve(planck_curve(x, 240),col=color[4], add=TRUE)
+    curve(planck_curve(x, 280),col=color[5], add=TRUE)
+
     points(avg0, Reduce("+", planck_curve(xran, 280))/length(planck_curve(xran, 280)), pch=16, col=color[5])
+    points(avg0, Reduce("+", planck_curve(xran, 240))/length(planck_curve(xran, 240)), pch=16, col=color[4])
 
     text(8.8, 21.5, label="T = 280 K", col=color[5], srt=10)
     text(7.5, 5.85, label="T = 240 K", col=color[4], srt=10)
 
-    text(9.5,offset[1], label=paste("T = ", round(temp_planck_curve(avg0, avg1), 2), "K"), srt=5, cex=0.75)
-    text(9.5,offset[2], label=paste("T = ", round(temp_planck_curve(avg0, avg2), 2), "K"), srt=5, cex=0.75)
+    text(9.5,offset[1], label=paste("T = ", round(temp_planck_curve(avg0, avg1),2), "K"), srt=5, cex=0.75)
+    text(9.5,offset[2], label=paste("T = ", round(temp_planck_curve(avg0, avg2),2), "K"), srt=5, cex=0.75)
     text(9.5,offset[3], label=paste("T = ", round(temp_planck_curve(avg0, avg3),2), "K"), srt=5, cex=0.75)
-
     legend("topright", col=color, pch=c(16,16), legend=leg)
-}
 
-modtran_plot1 <- function(fname){
-    plot(fname[,5], fname[,2], xlab="Water Pressure (mbar)", ylab="Altitude (km)",
-    main="Altitude and Water Pressure Correlation")
 }
 pdf("~/Downloads/modtran.pdf")
-modtran_plot(c(7,10), fname1, c(12.75,15,11.25), c("Water Vapor Scale = 1","Water Vapor Scale = 2","Water Vapor Scale = 0.5"))
-modtran_plot(c(7,10), fname2, c(11.5, 12.75, 14), c("-5 K Offset", "0 K Offset", "+5 K Offset"))
-modtran_plot1(fname3)
+modtran_plot(c(7,10), fname1, c(11.25,12.65,13.85,15), c("Water Vapor Scale = 0.5","Water Vapor Scale = 1.0","Water Vapor Scale = 1.5", "Water Vapor Scale = 2.0"))
+modtran_plot2(c(7,10), fname2, c(11.5,12.75,14,12), c("-5 K Offset", "0 K Offset", "+5 K Offset"))
+# modtran_plot1(fname3)
 graphics.off()
