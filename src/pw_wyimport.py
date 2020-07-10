4dc6d5b4dcbd9b086f364e575e212883670fb0bf
PharaohCola13 <spencer.riley@student.nmt.edu>
2020-07-10 11:15:11 -0600

    update

--- src/pw_wyimport.py
+++ src/pw_wyimport.py
@@ -5,7 +5,7 @@ from numpy import *
 from datetime import date as dte
 from datetime import datetime as dt
 
-sys.path.append("../../siphon-master/siphon/simplewebservice")
+sys.path.append("../../siphon-master/siphon-master/simplewebservice")
 import wyoming
 
 import os
