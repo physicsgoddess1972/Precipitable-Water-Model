f719c96eaed3fc1dc35f35e91e93d85b8694e4c9
spencerriley620 <spencer.riley@student.nmt.edu>
2020-08-05 11:50:17 -0600

    updates to pacman residual

--- setup.sh
+++ setup.sh
@@ -23,20 +23,19 @@ if [[ ${a_flag} ]]; then
 ## Install Packages
   echo -e "\e[96m ~~~~ Installing Packages ~~~~\e[0m"
 # System Requirements
-	wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
-	sudo dpkg -i apt.deb
 	sudo apt-get install gfortran libbz2-dev libv8-dev libcurl4-openssl-dev libxml2-dev libssl-dev unzip
-	sudo apt-get install zlib1g-dev libpcre3-dev liblzma-dev 
+	sudo apt-get install zlib1g-dev libpcre3-dev liblzma-dev
 	wget https://cran.rstudio.com/src/base/R-3/R-3.6.1.tar.gz
 	tar -zxf ./R-3.6.1.tar.gz
 	cd ./R-3.6.1/ && ./configure --with-readline=no --with-x=no && sudo make && sudo make install
 	cd ../ && sudo rm -r ./R-3.6.1/ && rm R-3.6.1.tar.gz
 ## R Package Requirements
-    sudo su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
-    sudo su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
-    sudo su - -c "R -e \"install.packages('devtools', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
-    sudo su - -c "R -e \"devtools::install_version('plotrix', version='3.5', repos='https://cran.rstudio.com/')\""
-    sudo su - -c "R -e \"install.packages('RColorBrewer', repos='https://cran.rstudio.com/')\""
+  sudo su - -c "R -e \"install.packages('crayon', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
+  sudo su - -c "R -e \"install.packages('argparse', repos='https://cran.rstudio.com/')\""
+  sudo su - -c "R -e \"install.packages('devtools', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
+  sudo su - -c "R -e \"devtools::install_version('plotrix', version='3.5', repos='https://cran.rstudio.com/')\""
+  sudo su - -c "R -e \"install.packages('RColorBrewer', repos='https://cran.rstudio.com/')\""
+	sudo su - -c "R -e \"devtools::install_github('PharaohCola13/pacres')\""
 elif [[ ${h_flag} ]]; then
 	echo "usage: setup.sh [-hcoma]"
 	echo ""
@@ -63,6 +62,8 @@ elif [[ ${i_flag} ]]; then
   sudo su - -c "R -e \"install.packages('devtools', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
   sudo su - -c "R -e \"devtools::install_version('plotrix', version='3.5', repos='https://cran.rstudio.com/')\""
 	sudo su - -c "R -e \"install.packages('RColorBrewer', repos='https://cran.rstudio.com/', dependencies=TRUE)\""
+	sudo su - -c "R -e \"devtools::install_github('PharaohCola13/pacres')\""
+
 elif [[ ${g_flag} ]]; then
 ### Download and Unzip Repository
   echo -e "\e[96m ~~~~ Downloading Repository ~~~~\e[0m"
--- src/model.r
+++ src/model.r
@@ -8,7 +8,7 @@
 ## Necessary Libraries for the script to run, for installation run install.sh
 library(argparse); library(crayon); library(RColorBrewer); library(plotrix)
 #library(randomcoloR); #library(Rpyplot);
-
+library(pacres)
 ## Custom Colors for cmd line features
 red 		<- make_style("red1")
 orange 		<- make_style("orange")
@@ -737,43 +737,15 @@ plots5 	<- function(..., overcast=args$overcast){
 ## Pacman Residual Plot
 plots6 	<- function(..., overcast=args$overcast){
     if(overcast){
-        exp_reg 	<- exp_regression(as.numeric(unlist(snsr_sky_calco)), avgo)
+				x <- as.numeric(unlist(snsr_sky_calco))
+				y <- avgo
         title 		<- "Pac-Man Residual of the Mean TPW and Temperature Model\nCondition: Overcast"
-    }else{
-        exp_reg 	<- exp_regression(as.numeric(unlist(snsr_sky_calc)), avg)
+		}else{
+				x <- as.numeric(unlist(snsr_sky_calc))
+				y <- avg
         title 		<- "Pac-Man Residual of the Mean TPW and Temperature Model\nCondition: Clear Sky"
     }
-	# residual quantities from the regression model
-	residual 	<- abs(resid(exp_reg$model))
-	# sequence used for angular position
-	t 			<- seq(40, 320, len=length(residual))
-	# Maximum radial distance
-	rmax 		<- max((residual), na.rm=TRUE)
-	# 6 equal divisions
-	divs 		<- seq(round(min(residual)), round(max(residual),0), len=6)
-	# Plots the residual against an angular position
-	polar.plot(0, rp.type="s",labels="",
-	radial.lim=c(0, round(rmax, 0)),show.grid=TRUE, show.grid.labels=FALSE,
-	main= title, show.radial.grid=FALSE, grid.col="black")
-
-	# Color Scheme for the rings
-	color1 <- "Yellow"; color2 <- "White"
-	draw.circle(0, 0, radius=divs[6], col=color1)
-	draw.circle(0, 0, radius=divs[5], col=color2)
-	draw.circle(0, 0, radius=divs[4], col=color1)
-	draw.circle(0, 0, radius=divs[3], col=color2)
-	draw.circle(0, 0, radius=divs[2], col=color1)
-
-	polar.plot(residual, t, rp.type="s",point.col="blue",point.symbols=16, add=TRUE)
-
-	text(divs[2] - 0.08, 0, labels=bquote(.(divs[2])*sigma))
-	text(divs[3] - 0.1, 0,  labels=bquote(.(divs[3])*sigma))
-	text(divs[4] - 0.1, 0,  labels=bquote(.(divs[4])*sigma))
-	text(divs[5] - 0.1, 0,  labels=bquote(.(divs[5])*sigma))
-	text(divs[6] - 0.1, 0,  labels=bquote(.(divs[6])*sigma))
-
-	polar.plot(c(0, round(rmax, 0)), c(min(t) - 10, min(t) - 10), lwd=1, rp.type="p",line.col="black", add=TRUE)
-	polar.plot(c(0, round(rmax, 0)), c(max(t) + 10, max(t) + 10), lwd=1, rp.type="p",line.col="black", add=TRUE)
+		pacres(x, log(y, base=exp(1)), title, "Yellow", "White")
 }
 
 ## Overcast Condition Percentage (bar)
