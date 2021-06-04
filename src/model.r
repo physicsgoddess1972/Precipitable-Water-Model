439470c2a25f70c2a3da55f864884ae52a68b27b
PharaohCola13 <academic@sriley.dev>
2021-06-03 19:26:47 -0600

    Update model.r

--- src/model.r
+++ src/model.r
@@ -74,9 +74,9 @@ quit_it <- function(){
 }
 
 ## Imports data from master_data.csv
-fname       <- read.table(file=paste(args$dir, "master_data_", args$id, ".csv"), sep=",", header=TRUE, strip.white=TRUE)
+fname       <- read.table(file="https://raw.githubusercontent.com/physicsgoddess1972/Precipitable-Water-Model/master/data/master_data_nm.csv", sep=",", header=TRUE, strip.white=TRUE)
 ## Imports sensor information from instruments.txt
-sensor 		<- suppressWarnings(read.csv(file=paste(args$dir, "instruments_", args$id, ".conf"), sep=","))
+sensor 		<- suppressWarnings(read.csv(file="https://raw.githubusercontent.com/physicsgoddess1972/Precipitable-Water-Model/master/data/instruments_nm.conf", sep=","))
 ## Pulls most recent data stamp for the purpose of adding date stamps to file names when plots are saved
 recent 		<- t(fname[1])[length(t(fname[1]))]
 
