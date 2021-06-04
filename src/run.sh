3cf1e09b44b73b08fe02c7ae6ff2fa8b6f72084d
Spencer Riley <spencer.riley@student.nmt.edu>
2021-06-03 21:26:54 -0600

    fixing CI issues

--- src/run.sh
+++ src/run.sh
@@ -11,10 +11,11 @@
 # cd ../
 
 N=100
+id="${5:-$I}"
 mlN="${3:-$N}"
 ts="\e[92mTime Series plots saved to figs/results/\e[0m"
 ap="\e[92mAnalytical plots saved to figs/results/\e[0m"
-in="\e[92mIndividual Sensor plots haved to figs/results/\e[0m"
+in="\e[92mIndividual Sensor plots saved to figs/results/\e[0m"
 ch="\e[92mCharts saved to figs/results/\e[0m"
 po="\e[92mPoster plots saved to figs/results/\e[0m"
 pa="\e[92mPac-man plots saved to figs/results/\e[0m"
@@ -24,50 +25,50 @@ while getopts "oahcs" opt; do
 	case "${opt}" in
 		c)
 				echo -e "\e[96m ~~~~ Clear Sky ~~~~\e[0m"
-				Rscript model.r --set t &> /dev/null & Rscript model.r --set a &> /dev/null
+				Rscript model.r --set t -id $id &> /dev/null & Rscript model.r --set a -id $id &> /dev/null
 				echo -e "[Clear Sky] ${ts}"
 				echo -e "[Clear Sky] ${ap}"
-				Rscript model.r --set i &> /dev/null & Rscript model.r --set c &> /dev/null
+				Rscript model.r --set i -id $id &> /dev/null & Rscript model.r --set c -id $id &> /dev/null
 				echo -e "[Clear Sky] ${in}"
 				echo -e "[Clear Sky] ${ch}"
-				Rscript model.r --poster &> /dev/null & Rscript model.r --pacman &> /dev/null
+				Rscript model.r --poster -id $id &> /dev/null & Rscript model.r --pacman  -id $id &> /dev/null
 				echo -e "[Clear Sky] ${po}"
 				echo -e "[Clear Sky] ${pa}"
 				echo -e "\e[96m ~~~~ Complete ~~~~\e[0m" && exit 0;;
 		o)
 				echo -e "\e[96m ~~~~ Overcast ~~~~\e[0m"
-				Rscript model.r --set t -o &> /dev/null & Rscript model.r --set a -o &> /dev/null
+				Rscript model.r --set t -o -id $id &> /dev/null & Rscript model.r --set a -o -id $id&> /dev/null
 				echo -e "[Overcast] ${ts}"
 				echo -e "[Overcast] ${ap}"
-				Rscript model.r --set c &> /dev/null & Rscript model.r --set i -o &> /dev/null
+				Rscript model.r --set c -id $id &> /dev/null & Rscript model.r --set i -o -id $id &> /dev/null
 				echo -e "[Overcast] ${ch}"
 				echo -e "[Overcast] ${in}"
-				Rscript model.r --poster &> /dev/null & Rscript model.r --pacman -o &> /dev/null
+				Rscript model.r --poster -id $id &> /dev/null & Rscript model.r --pacman -o -id $id &> /dev/null
 				echo -e "[Overcast] ${po}"
 				echo -e "[Overcast] ${pa}"
 				echo -e "\e[96m ~~~~ Complete ~~~~\e[0m" && exit 0;;
 		a)
 				echo -e "\e[96m ~~~~ All Plots ~~~~\e[0m"
-				Rscript model.r --set t -o &> /dev/null & Rscript model.r --pacman -o &> /dev/null
+				Rscript model.r --set t -o -id $id &> /dev/null & Rscript model.r --pacman -o -id $id &> /dev/null
 				echo -e "[Overcast] ${ts}"
 				echo -e "[Overcast] ${pa}"
-				Rscript model.r --set a -o &> /dev/null & Rscript model.r --set i -o &> /dev/null
+				Rscript model.r --set a -o -id $id &> /dev/null & Rscript model.r --set i -o -id $id &> /dev/null
 				echo -e "[Overcast] ${ap}"
 				echo -e "[Overcast] ${in}"
-				Rscript model.r --set t &> /dev/null & Rscript model.r --pacman &> /dev/null
+				Rscript model.r --set t -id $id &> /dev/null & Rscript model.r --pacman -id $id &> /dev/null
 				echo -e "[Clear Sky] ${ts}"
 				echo -e "[Clear Sky] ${pa}"
-				Rscript model.r --set a &> /dev/null & Rscript model.r --set i &> /dev/null
+				Rscript model.r --set a -id $id &> /dev/null & Rscript model.r --set i -id $id &> /dev/null
 				echo -e "[Clear Sky] ${ap}"
 				echo -e "[Clear Sky] ${in}"
-				Rscript model.r --poster &> /dev/null & Rscript model.r --set c &> /dev/null
+				Rscript model.r --poster -id $id &> /dev/null & Rscript model.r --set c -id $id &> /dev/null
 				echo -e "[All] ${po}"
 				echo -e "[All] ${ch}"
-				Rscript model.r --data -ml &> /dev/null
+				Rscript model.r --data -ml -id $id &> /dev/null
 				echo -e "\e[92mMachine Learning Data saved\e[0m"
 				echo -e "\e[96m ~~~~ Complete ~~~~\e[0m" && exit 0;;
 		s)
-				Rscript model.r --data -ml &> /dev/null
+				Rscript model.r --data -ml -id $id &> /dev/null
 				echo -e "\e[92mMachine Learning Data saved\e[0m"
 				python3 ./ml/svm/class_svm_tf.py -dfile "../data/ml/" -N $mlN -ffile "../figs/ml/"
 				echo -e "\e[96m ~~~~ Complete ~~~~\e[0m" && exit 0;;
@@ -82,6 +83,8 @@ while getopts "oahcs" opt; do
 				echo "   -s         runs SVM Module";
 				echo "   -N         number of random states for SVM Module [Default: 100]"
 				exit 0;;
+		*)
+		  echo "Invalid option"
 	esac
 done
 if [ "${opt}" = "?" ]; then
