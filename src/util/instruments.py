101d048efadecb0a26e5beabd15d3c9b72c8b30c
PharaohCola13 <academic@sriley.dev>
2021-06-12 14:39:01 -0600

    Fixed issue with string formatting

--- src/util/instruments.py
+++ src/util/instruments.py
@@ -5,9 +5,9 @@ parser.add_argument("-I", type=str, help="ID for data import", dest="I")
 
 args = parser.parse_args()
 
-with open("../data/%s/instruments.yml".format(args.I)) as f:
+with open("../../data/{}/instruments.yml".format(args.I)) as f:
     my_dict = yaml.safe_load(f)
-with open("../data/%s/instruments.conf".format(args.I), 'w', newline='') as csvfile:
+with open("../../data/{}/instruments.conf".format(args.I), 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=my_dict[0]['sensor'].keys())
     writer.writeheader()
     for data in my_dict:
