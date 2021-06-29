6a49fd9360eb382cafa80fad8b7983a607a79eb5
PharaohCola13 <academic@sriley.dev>
2021-06-28 19:59:51 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir -p figs/results/; ls
-touch /data/master_data.csv
+mkdir -p figs/results/; mv /src/ ./
+ls data/
+touch data/master_data.csv
 cd src/util/; python3 instruments.py; python3 data_import.py
 cd ../; bash run.sh -a
 cd ../;  rm -r src/ && rm data/instruments.conf
