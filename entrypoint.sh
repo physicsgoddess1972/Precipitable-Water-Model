b059db6a5bcad3510edfbac6ca8fbc33f22d3ace
Spencer Riley <spencer.riley@student.nmt.edu>
2021-07-01 10:55:01 -0600

    Testing new method of getting src files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-mkdir -p figs/results/ src/ src/util
-ls
-mv main/src/model.r src/model.r
-mv main/src/run.sh src/run.sh
-mv main/src/util/*.py src/util/
-rm -r main
+mkdir -p figs/results/
 touch data/master_data.csv
 cd src/util/; python3 instruments.py; python3 data_import.py
 cd ../; bash run.sh -a
