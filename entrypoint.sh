9255460a74eff44249ba6a875d7f72e2047de190
PharaohCola13 <academic@sriley.dev>
2021-07-04 12:32:26 -0600

    Potential fix for data import issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ mkdir -p figs/results/; mv /src/ src/
 touch data/master_data.csv
 cd src/util/; python3 instruments.py; python3 data_import.py
 cd ../; bash run.sh -a
+chmod 755 figs/
 cd ../;  rm -r src/ && rm data/instruments.conf
