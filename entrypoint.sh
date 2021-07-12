06a1f816527fd94d7b84b98816f308de9b685f20
Spencer Riley <spencer.riley@student.nmt.edu>
2021-07-11 18:33:41 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ mkdir -p figs/results/; mv /src/ src/
 touch data/master_data.csv
 cd src/util/; python3 instruments.py; python3 data_import.py
 cd ../; bash run.sh -a
-sudo chmod -R ug+w figs/
+chmod -R ug+w figs/
 cd ../;  rm -r src/ && rm data/instruments.conf
