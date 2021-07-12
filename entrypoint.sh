33008b04a12d80132b3f6f119a78ac6f37a6f7d2
PharaohCola13 <academic@sriley.dev>
2021-07-11 18:07:49 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ mkdir -p figs/results/; mv /src/ src/
 touch data/master_data.csv
 cd src/util/; python3 instruments.py; python3 data_import.py
 cd ../; bash run.sh -a
-chmod 755 figs/
+sudo chmod -R ug+w figs/
 cd ../;  rm -r src/ && rm data/instruments.conf
