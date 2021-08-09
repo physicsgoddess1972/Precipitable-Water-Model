12c4af697329e11ecd929e4d10fdc0519e256bf2
PharaohCola13 <academic@sriley.dev>
2021-08-09 10:57:37 -0600

    Update sidenav.js

--- docs/assets/js/sidenav.js
+++ docs/assets/js/sidenav.js
@@ -2,11 +2,9 @@ var myObj = {}
 
 myObj.anc = function(){
     $.get("assets/external/side_nav.html", function(data){
+        console.log(document.location.pathname);
         var path = document.location.pathname;
         var page1 = path.split("/").pop().replace(".", "-");
-        console.log(path);
-        console.log(page1);
-        console.log(document.getElementById(page1));
         document.getElementById(page1).parentElement.setAttribute('class', 'submenu show');
         var page2 = document.getElementById(page1).classList;
         page2.add("mdl-navigation__link--current");
