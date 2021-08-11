1232568806f0fc7e9b81b0dcfdd949e8729331e1
PharaohCola13 <academic@sriley.dev>
2021-08-11 11:30:54 -0600

    Update header.js

--- docs/assets/js/header.js
+++ docs/assets/js/header.js
@@ -2,6 +2,7 @@
     $.get("assets/external/head.html", ).done(function (data) {
         data = data.replace(/file_name/g, fname);
         data = data.replace(/branch/g, bname);
-        $("#content").html(data);
+        $('head').append(data);
+//         $("#content").html(data);
     });
 })();
