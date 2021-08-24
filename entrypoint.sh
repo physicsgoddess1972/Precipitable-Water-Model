0055c96e16a80a366d42afef3af6ec4bb2d1d6d9
Spencer Riley <spencer.riley@student.nmt.edu>
2021-08-24 12:56:56 -0600

    minor fix with new updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mkdir -p figs/results/; mv /src/ src/
-touch data/master_data.csv
-cd src/util/; python3 instruments.py; python3 data_import.py
+if [[ ! -e data/master_data.csv ]]; then
+  touch data/master_data.csv
+fi
+cd src/util/; python3 data_import.py
 cd ../; bash run.sh -a
-cd ../;  rm -r src/ && rm data/instruments.conf
+cd ../;  rm -r src/
+
