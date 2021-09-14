74e8a3337285a6b0ad094795f5df213de9cff069
Spencer Riley <spencer.riley@student.nmt.edu>
2021-09-14 15:17:33 -0600

    appendix B plot

--- src/util/paper/cool.sh
+++ src/util/paper/cool.sh
@@ -1,5 +1,5 @@
 #! bin/bash
-for i in `seq 1 10000`; do
-  Rscript paper_figures.r >>  ./rsme_riley_75.txt
-  echo -e "\e[1A\e[K$i out of 10000"
+for i in `seq 1 5000`; do
+  Rscript paper_figures.r >>  ./rsme_riley_55.txt
+  echo -e "\e[1A\e[K$i out of 5000"
 done
