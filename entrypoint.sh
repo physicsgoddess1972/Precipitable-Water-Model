ea1fe62e7036b6f3db664b4026d21a370f7f826e
PharaohCola13 <academic@sriley.dev>
2021-11-09 13:39:41 -0700

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ mkdir -p figs/results/; mv /src/ src/
 if [[ ! -e data/master_data.csv ]]; then
   touch data/master_data.csv
 fi
-cd src/util/; python3 pmat_import.py
+cd src/; python3 pmat_import.py
 cd ../; bash run.sh -a
 cd ../;  rm -r src/
 
