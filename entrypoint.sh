c03a5c2c4973e4cf5d6d046d3b4b85e047c31ff4
PharaohCola13 <academic@sriley.dev>
2021-11-11 08:42:43 -0700

    workflow fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@ mkdir -p figs/results/; mv /src/ src/
 if [[ ! -e data/master_data.csv ]]; then
   touch data/master_data.csv
 fi
-cd src/; python3 pmat_import.py
+cd src/
+echo "Starting Data Aquistion"
+python3 pmat_import.py
+echo "Running PMAT"
 bash run.sh -a
 cd ../;  rm -r src/
 
