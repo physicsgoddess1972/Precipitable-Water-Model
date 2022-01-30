a6caf47257bde391f1b850c40e579bf3391d13d5
PharaohCola13 <academic@sriley.dev>
2022-01-30 13:20:55 -0700

    pmat documentation update

--- src/pmat_logging.r
+++ src/pmat_logging.r
@@ -1,7 +1,7 @@
-#' @title Precipitable Water Model Analysis Tool: Logging
-#' @file pmat_logging.r
-#' @author Spencer Riley
-#' @brief functions for logging and internal error handling
+#' :file: pmat_logging.r
+#' :module: Precipitable Water Model Analysis Tool: Logging
+#' :synopsis: functions for logging and internal error handling
+#' :author: Spencer Riley
 
 ## warning codes
 a01 <- "Warn a01: Insufficient data for accurate analysis"
@@ -12,13 +12,10 @@ D01 <- "Error D01: Insufficient clear sky/overcast data"
 F01 <- "Error F01: master_data.csv is not found"
 F02 <- "Error F02: _pmat.yml is not found"
 
-#' @title suppress
-#' @description suppresses messages
-#' @param obj
-#' @param verbose
-#' @export
-
 suppress <- function(obj, verbose=config[[3]]$logging[[1]]$verbose){
+  #' :details: suppresses messages
+  #' :param obj:
+  #' :param verbose:
   if (verbose == "none"){
     suppressMessages(obj)
   } else {
@@ -26,20 +23,15 @@ suppress <- function(obj, verbose=config[[3]]$logging[[1]]$verbose){
   }
 }
 
-
-#' @title error
-#' @description function designed to print error codes
-#' @param code the error code
-#' @export
 error <- function(code){
+  #' :details: function designed to print error codes
+  #' :param code: the error code
   cat(red(paste(code, "\n"))); quit()
 }
 
-#' @title warning
-#' @description function designed to print warning codes
-#' @param code the warning code
-#' @export
 warning <- function(code){
+  #' :details: function designed to print warning codes
+  #' :param code: the warning code
   message(orange(paste(code, "\n")))
 }
 
--- src/pmat_utility.r
+++ src/pmat_utility.r
@@ -1,12 +1,10 @@
-#' @title Precipitable Water Model Analysis Tool: utility
-#' @file pmat_utility.r
-#' @author Spencer Riley
-#' @brief general functions for PMAT
+#' :file: pmat_utility.r
+#' :module: Precipitable Water Model Analysis Tool: Utility
+#' :synopsis: general functions for PMAT
+#' :author: Spencer Riley
 
-#' @title startup
-#' @description shows title banner for program
-#' @export
 startup <- function(){
+    #' :details: shows title banner for program
     suppress(message(bold(cloudblue(paste(replicate(65, "-"), collapse="")))))
     suppress(message(bold(cloudblue("\t\t   Precipitable-water Model Analysis Tool   \t\t\t"))))
     suppress(message(bold(cloudblue(paste(replicate(65, "-"), collapse="")))))
@@ -15,42 +13,34 @@ startup <- function(){
     suppress(message(bold(cyan("\t\t>>>>>>>>> Program Start <<<<<<<<\n"))))
 }
 
-#' @title closing
-#' @description cleans up files and ends the program
-#' @export
 closing <- function(){
+    #' :details: cleans up files and ends the program
     ## Ends the script
     if(file.exists("Rplots.pdf")){file.remove("Rplots.pdf")}
 # End of program
     suppress(message(bold(cyan("\n\t\t>>>>>>> Program Complete <<<<<<<")))); quit()
 }
 
-#' @title save
-#' @description A general function that will save plots
-#' @param func the plotting function that will be saved
-#' @param name the name of the file with the plots
-#' @return A pdf of the plots
-#' @export
 save <- function(func, name){
+    #' :details: A general function that will save plots
+    #' :param func: the plotting function that will be saved
+    #' :param name: the name of the file with the plots
+    #' :return: A pdf of the plots
 	pdf(name);func;invisible(graphics.off())
 }
 
-#' @title reset_time
-#' @description A function that sets the time to 00:00:00
-#' @param datetime a Date or datetime object
-#' @return A datetime object with time 00:00:00
-#' @export
 reset_time <- function(datetime){
+    #' :details: A function that sets the time to 00:00:00
+    #' :param datetime: a Date or datetime object
+    #' :return: A datetime object with time 00:00:00
     return(as.POSIXct(paste(substr(datetime, 1, 11),"00:00:00",sep=" "), format="%Y-%m-%d %H:%M:%S"))
 }
 
-#' @title time_axis_init
-#' @description A function that calculates the min, max, and position of the tick marks for
-#' the time series.
-#' @param date A date or datetime object
-#' @return The max, min, and tick mark positions
-#' @export
 time_axis_init <- function(date){
+    #' :details: A function that calculates the min, max, and position of the tick marks for
+    #' the time series.
+    #' :param date: A date or datetime object
+    #' :return: The max, min, and tick mark positions
     xmin <- reset_time(paste(substr(date[[1]], 1, 8),"01",sep=""))
     dm <- ifelse(round(as.integer(substr(date[[length(date)]], 9, 10))/20) > 1, 1, 0)
     xmax <- reset_time(paste(substr(date[[length(date)]], 1, 5),
@@ -63,11 +53,9 @@ time_axis_init <- function(date){
     return(list(c(xmin, xmax), ticks.at))
 }
 
-#' @title time_axis
-#' @description A function that sets the x-axis format for time series plots
-#' @param date A date or datetime object
-#' @export
 time_axis <- function(datetime){
+    #' :details: A function that sets the x-axis format for time series plots
+    #' :param date: A date or datetime object
     # defines major and minor tick marks for the x-axis and their position
     ticks.at <- time_axis_init(datetime)[2][[1]]
     if (length(ticks.at) > 3){
@@ -83,13 +71,11 @@ time_axis <- function(datetime){
     minor.tick(nx=0, ny=2, tick.ratio=0.5, x.args = list(), y.args = list())
 }
 
-#' @title stnd_title
-#' @description A function that generates the title based on
-#' the sky condition and description of the plot
-#' @param des the description of the plot
-#' @param overcast the sky condition
-#' @return a title string
-#' @export
 stnd_title <- function(des, overcast){
+    #' :details: A function that generates the title based on
+    #' the sky condition and description of the plot
+    #' :param des: the description of the plot
+    #' :param overcast: the sky condition
+    #' :return: a title string
 	return(sprintf("%s\nCondition: %s", des, ifelse(overcast,"Overcast", "Clear Sky")))
 }
