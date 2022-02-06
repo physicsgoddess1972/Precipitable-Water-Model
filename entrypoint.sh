5f25a24f32164b4870eb7bb4ba76b01771aeb375
Spencer Riley <spencer.riley@student.nmt.edu>
2022-02-05 18:52:37 -0700

    manual update to deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ if [[ ! -e data/master_data.csv ]]; then
   touch data/master_data.csv
 fi
 cd src/
-echo "Starting Data Aquistion"
-python3 pmat_import.py
-echo "Running PMAT"
 Rscript pmat_run.r --dir ./data/ -all
 Rscript pmat_run.r --dir ./data/ -all -o
 cd ../;  rm -r src/
