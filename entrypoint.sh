a106a206b134800ed6a01cb1be4664a152e52e6e
PharaohCola13 <academic@sriley.dev>
2022-02-20 11:39:23 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -p figs/results/; mv /src/ src/
+mkdir -p out/figs/ out/data/; mv /src/ src/
 if [[ ! -e data/master_data.csv ]]; then
   touch data/master_data.csv
 fi
 cd src/
-Rscript pmat_run.r --dir ./data/ -all
-Rscript pmat_run.r --dir ./data/ -all -o
+Rscript pmat_run.r --dir ./data/ -all -u
+Rscript pmat_run.r --dir ./data/ -all -u -o
 cd ../;  rm -r src/
 
