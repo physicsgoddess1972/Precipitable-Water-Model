ad994e4f91f4ea0f254c5369ecd467c8e7a15b21
PharaohCola13 <academic@sriley.dev>
2022-06-14 07:41:26 -0600

    Delete header.js

--- docs/assets/js/header.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    $.get("assets/external/head.html", ).done(function (data) {
-        data = data.replace(/file_name/g, fname);
-        data = data.replace(/branch/g, bname);
-        $('head').append(data);
-//         $("#content").html(data);
-    });
-})();
